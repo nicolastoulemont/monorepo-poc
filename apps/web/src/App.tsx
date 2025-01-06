86143c6a251aa06bf4443450698f09ee9b7137b6
ntoulemont <n.toulemont@gmail.com>
2025-01-06 13:58:07 +0100

    feat: import ui kit styles

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
+		<div className="flex flex-col items-center justify-center h-screen w-full">
 			<Button onClick={() => setCount((count) => count + 1)}>
 				count is {count}
 			</Button>
