d286c960bc1ba3e93645790ee2a43be40d237809
ntoulemont <n.toulemont@gmail.com>
2025-01-06 14:46:38 +0100

    feat: move the common vitest config to shared config package

---
+++ configs/vitest/vitest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('vitest').UserConfig} */
+export default {
+	globals: true,
+	// Enable  expectTypeOf(1).toEqualTypeOf(2) fns
+	// https://github.com/vitest-dev/vitest/tree/main/examples/typecheck
+	typecheck: {
+		enabled: true,
+	},
+	browser: {
+		enabled: true,
+		name: "chromium",
+		provider: "playwright",
+		// https://playwright.dev
+		providerOptions: {},
+	},
+};
