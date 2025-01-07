ad726e9fde87dde7ece3b7a7739033d4285ab076
Nicolas Toulemont <nicolas.toulemont@gorgias.com>
2025-01-07 15:46:30 +0100

    feat: working turbo repo setup with dependencies chain respected

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ export type ButtonProps = ComponentProps<"button">
 export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
-      className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 active:bg-green-700 transition-colors"
+      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 active:bg-blue-700 transition-colors"
       {...props}
     >
       {children}
