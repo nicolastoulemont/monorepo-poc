fccaef0b1e09e59c7de802115253316e449f69ec
ntoulemont <n.toulemont@gmail.com>
2025-01-07 10:07:58 +0100

    feat: added nx to project

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,13 @@
 import "@poc/one/dist/index.css"
+import "@poc/two/dist/index.css"
 import { ComponentContainer } from "@poc/one"
+import { ComponentContainer as ComponentContainer2 } from "@poc/two"
 
 function App() {
   return (
     <div className="min-h-screen w-full">
       <ComponentContainer />
+      <ComponentContainer2 />
     </div>
   )
 }
